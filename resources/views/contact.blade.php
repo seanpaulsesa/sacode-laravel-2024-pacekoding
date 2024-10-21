f8aab47021ebefba8230e58334b5271a15f9c3d1
Samuel Bosawer <zam.bosawer@gmail.com>
2024-10-21 20:02:14 +0900

    Belajar Router

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Contact</title>
+</head>
+<body>
+<h1>Contact</h1>
+<p>
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempora eveniet quibusdam delectus maiores temporibus aliquam repudiandae assumenda mollitia consequatur nam eligendi iure atque pariatur vero in id eos omnis minus, veniam dolores neque laboriosam architecto! Aliquid aspernatur ullam tempore est quasi. Doloremque possimus nesciunt debitis animi aliquid totam, culpa nisi.
+
+</p>
+
+
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -6,14 +6,19 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/about/{nama}', function ($nama) {
+    return view('about', compact('nama'));
+});
+
 Route::get('/about', function () {
     return view('about');
 });
 
+
 Route::get('/contact', function () {
-    return 'Halaman Contact';
+    return view('contact');
 });
 
 Route::get('/blog', function () {
-    return 'Halaman blog';
+    return view('blog');
 });
